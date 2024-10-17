e39191abfc155cacf97bfa12e3a2aa3143013641
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-10-17 14:01:23 +0200

    Refactor code to use stack.h and libft.h in main.c, push_swap.h, sort.h, and sort_small.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 13:39:44 by sfarren           #+#    #+#             */
-/*   Updated: 2024/10/17 13:54:08 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/10/17 13:55:14 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack_node *stack_a;
+	t_stack_node	*stack_a;
 	// t_stack_node *stack_b;
 
 	stack_a = NULL;
 	// stack_b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
-	init_stack_a(&stack_a,argc, argv);
+	init_stack_a(&stack_a, argc, argv);
 	if (!stack_sorted(stack_a))
 	{
 		if (stack_len(stack_a) == 2)
---
+++ src/sort/sort_small.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_small.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 19:40:20 by sfarren           #+#    #+#             */
+/*   Updated: 2024/10/17 14:00:09 by sfarren          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../stack.h"
+
+char	*sort_two(t_stack_node *stack, int *arr, int min, int max)
+{
+	static char	*instructions;
+
+	if (arr[0] == max && arr[1] == min)
+	{
+		swap(stack);
+		return (instructions = "sa\n");
+	}
+	return (instructions = "none required\n");
+}
+
+char	*sort_three(t_stack_node *stack, int *arr, int min, int max)
+{
+	char	*instructions;
+
+	if (arr[0] == min && arr[1] == max)
+	{
+		swap(stack);
+		rotate(stack);
+		return (instructions = "sa,ra\n");
+	}
+	if (arr[0] == max && arr[1] == min)
+	{
+		rotate(stack);
+		return (instructions = "ra\n");
+	}
+	if (arr[0] == max && arr[2] == min)
+	{
+		swap(stack);
+		reverse_rotate(stack);
+		return (instructions = "sa,rra\n");
+	}
+
+	return (instructions = "");
+}
