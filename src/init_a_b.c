0bc1c7ef4eb8f6e8e35b9a6cfd733623af3a8e03
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-10-18 14:38:06 +0200

    Refactor code to update file associations in .vscode/settings.json

--- src/init_a_b.c
+++ src/init_a_b.c
@@ -6,7 +6,7 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 21:20:12 by sfarren           #+#    #+#             */
-/*   Updated: 2024/10/17 21:46:28 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/10/18 14:20:04 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,68 @@ static void	set_target_a(t_stack_node *stack_a, t_stack_node *stack_b)
 {
 	t_stack_node	*current_b;
 	t_stack_node	*target_node;
-	int				target_index;
+	long			target_index;
 
 	while (stack_a)
 	{
-
+		target_index = LONG_MIN;
+		current_b = stack_b;
+		while (current_b)
+		{
+			if (current_b->value < stack_a->value
+				&& current_b->index > target_index)
+			{
+				target_index = current_b->value;
+				target_node = current_b;
+			}
+			current_b = current_b ->next;
+		}
+		if (target_index == LONG_MIN)
+			stack_a->target = find_max(stack_b);
+		else
+			stack_a->target = target_node;
+		stack_a = stack_a->next;
 	}
+}
 
+static void	calculate_cost_a(t_stack_node *stack_a, t_stack_node *stack_b)
+{
+	int	len_a;
+	int	len_b;
+
+	len_a = stack_len(stack_a);
+	len_b = stack_len(stack_b);
+	while (stack_a)
+	{
+		stack_a->cost = stack_a->index;
+		if (!(stack_a->above_median))
+			stack_a->cost = len_a - (stack_a->index);
+		if (stack_a->target->above_median)
+			stack_a->cost += stack_a->target->index;
+		else
+			stack_a->cost += len_b - (stack_a->target->index);
+		stack_a = stack_a->next;
+	}
+}
+
+void	set_lowest_cost(t_stack_node *stack)
+{
+	long			lowest_cost;
+	t_stack_node	*lowest_cost_node;
+
+	if (!stack)
+		return ;
+	lowest_cost = LONG_MAX;
+	while (stack)
+	{
+		if (stack->cost < lowest_cost)
+		{
+			lowest_cost = stack->cost;
+			lowest_cost_node = stack;
+		}
+		stack = stack->next;
+	}
+	lowest_cost_node->lowest_cost = true;
 }
 
 void	initialise_nodes_a(t_stack_node *stack_a, t_stack_node *stack_b)
@@ -52,5 +107,5 @@ void	initialise_nodes_a(t_stack_node *stack_a, t_stack_node *stack_b)
 	current_index(stack_b);
 	set_target_a(stack_a, stack_b);
 	calculate_cost_a(stack_a, stack_b);
-	set_cheapest(stack_a);
+	set_lowest_cost(stack_a);
 }
--- src/sort/sort_big.c
+++ src/sort/sort_big.c
@@ -6,11 +6,48 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 21:04:20 by sfarren           #+#    #+#             */
-/*   Updated: 2024/10/17 21:26:06 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/10/18 14:37:07 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+static void	rotate_both(t_stack_node **stack_a,
+						t_stack_node **stack_b,
+						t_stack_node *lowest_cost_node)
+{
+	while (*stack_b != lowest_cost_node->target
+		&& *stack_a != lowest_cost_node)
+		rr(stack_a, stack_b);
+	current_index(*stack_a);
+	current_index(*stack_b);
+}
+
+static void	rev_rotate_both(t_stack_node **stack_a,
+								t_stack_node **stack_b,
+								t_stack_node *lowest_cost_node)
+{
+	while (*stack_b != lowest_cost_node->target
+		&& *stack_a != lowest_cost_node)
+		rrr(stack_a, stack_b);
+	current_index(*stack_a);
+	current_index(*stack_b);
+}
+
+static void	move_a_to_b(t_stack_node **stack_a, t_stack_node **stack_b)
+{
+	t_stack_node	*low_cost_node;
+
+	low_cost_node = find_lowest_cost(*stack_a);
+	if (low_cost_node->above_median && low_cost_node->target->above_median)
+		rotate_stacks(stack_a, stack_b, low_cost_node);
+	else if (!(low_cost_node->above_median)
+		&& !(low_cost_node->target->above_median))
+		reverse_rotate_stacks(stack_a, stack_b, low_cost_node);
+	set_push(stack_a, low_cost_node, 'a');
+	set_push(stack_b, low_cost_node->target, 'b');
+	pb(stack_a, stack_b);
+}
 
 void	sort_big(t_stack_node **stack_a, t_stack_node **stack_b)
 {
