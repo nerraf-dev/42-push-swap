5cd83c524ed309d8e95fcc7b501060e4b839a1e4
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2025-03-30 14:49:40 +0200

    Remove tester directory and associated scripts for cleaner project structure

--- tester/utils.py
+++
@@ -1,9 +0,0 @@
-import sys
-from config import COLOUR
-
-def print_error_exit(msg):
-    """
-    Prints an error message in red color and exits the program.
-    """
-    print(COLOUR["RED"], msg, COLOUR["ENDC"])
-    sys.exit(1)
